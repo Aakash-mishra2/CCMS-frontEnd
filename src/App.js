1417b52a6c847cdb3dc829fbb5ef0fb93726f710
Aakash Mishra <aakashmishra.281@gmail.com>
2023-06-19 14:14:40 +0530

    updated routes

--- src/App.js
+++ src/App.js
@@ -22,23 +22,24 @@ export const App = () => {
   if (log_In) {
     routes = (
       <Routes>
-        <Route path="/" element={< Citizens />} />
+        <Route path="/" element={< Authenticate />} />
+        <Route path="/citizens" element={<Citizens />} />
         <Route path="/cases/new" element={<NewCases />} />
         <Route path="/:uid/cases" element={< RegisteredCases />} />
         <Route path="/update/:caseID" element={<UpdateCases />} />
-        <Route path="/users/authenticate" element={<Navigate to="/" />} />
+        <Route path="/" element={<Navigate to="/citizens" />} />
       </Routes>
     )
   }
   else {
     routes = (
       <Routes>
-        <Route path="/" element={< Citizens />} />
-        <Route path="/users/authenticate" element={<Authenticate />} />
-        <Route path="/users/authenticate" element={<Navigate to="/users/authenticate" />} />
-        <Route path="/cases/new" element={<Navigate to="/users/authenticate" />} />
-        <Route path="/:uid/cases" element={<Navigate to="/users/authenticate" />} />
-        <Route path="/update/:caseID" element={<Navigate to="/users/authenticate" />} />
+        <Route path="/" element={< Authenticate />} />
+        <Route path="/citizens" element={<Citizens />} />
+        <Route path="/citizens" element={<Navigate to="/" />} />
+        <Route path="/cases/new" element={<Navigate to="/" />} />
+        <Route path="/:uid/cases" element={<Navigate to="/" />} />
+        <Route path="/update/:caseID" element={<Navigate to="/" />} />
       </Routes>
     )
   }
